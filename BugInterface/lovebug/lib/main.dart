c1ad9b32a3102bf571422d006a437f6e58a5d157
Juniper Mixon <zemchar3@gmail.com>
2023-09-08 21:57:35 -0500

    Create basic settings page

--- BugInterface/lovebug/lib/main.dart
+++ BugInterface/lovebug/lib/main.dart
@@ -1,125 +1,424 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart'; // Import the provider package
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (_) => ThemeProvider(),
+    child: MyApp(),
+  ));
+}
+class ThemeProvider extends ChangeNotifier {
+  Color currentTheme = Colors.pink;
+
+  void changeTheme(Color newTheme) {
+    currentTheme = newTheme;
+    notifyListeners(); // Notify listeners of the change
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Love Bug',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: themeProvider.currentTheme),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainPage(title: 'Love Bug'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+mixin theming {
+  static Color currentTheme = Colors.pink;
+  static var themeMapping = {
+    'Pink': Colors.pink,
+    'Green': Colors.green,
+    'Blue': Colors.blue,
+    'Purple': Colors.purple,
+    'Orange': Colors.orange,
+    'Red': Colors.red,
+  };
+  static String currentThemeName = 'Pink';
+}
+
+// Main Page
+class MainPage extends StatefulWidget {
+  const MainPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageState extends State<MainPage> {
+  final List<Widget> _children = [
+    const Home(),
+    const Games(),
+    const Gallery(),
+    Settings()
+  ];
+  int _index =
+      0; // Make sure this is outside build(), otherwise every setState will change the value back to 0
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  @mustCallSuper
+  void initState() {
+    print('MainPage InitState');
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Love Bug'),
+          centerTitle: true,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
+        body: _children[_index],
+        bottomNavigationBar: BottomNavigationBar(
+            onTap: (tappedItemIndex) => setState(() {
+                  _index = tappedItemIndex;
+                }),
+            currentIndex: _index,
+            items: [
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.home_outlined),
+                  label: 'Home',
+                  backgroundColor: Theme.of(context).colorScheme.scrim),
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.gamepad_outlined),
+                  label: 'Games',
+                  backgroundColor: Theme.of(context).colorScheme.scrim),
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.photo_album_outlined),
+                  label: 'Gallery',
+                  backgroundColor: Theme.of(context).colorScheme.scrim),
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.settings_outlined),
+                  label: 'Settings',
+                  backgroundColor: Theme.of(context).colorScheme.scrim),
+            ]));
+  }
+}
+
+// Home Page
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: const Text('Home Page'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// Games Page
+class Games extends StatelessWidget {
+  const Games({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Games Page'),
+    );
+  }
+}
+
+// Gallery Page
+class Gallery extends StatelessWidget {
+  const Gallery({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Gallery Page'),
+    );
+  }
+}
+
+// Settings Page
+class Settings extends StatefulWidget {
+  Settings({super.key});
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  String _notifBoxValue = 'On';
+  final TextEditingController _nameController = TextEditingController();
+  late ThemeProvider themeProvider; // Declare a ThemeProvider variable
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // Access the ThemeProvider using the context provided by the build method
+    themeProvider = Provider.of<ThemeProvider>(context);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text('Your Bug Code: ${_BugCode.BugCode}', style: TextStyle(fontSize: 20)),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Notifications', style: TextStyle(fontSize: 30)),
+                SizedBox(width: 20),
+                SizedBox(width: 250, height: 70, child:
+                InputDecorator(
+                    decoration: InputDecoration(
+                      border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
+                    ), child:
+                DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    icon: const Icon(Icons.notifications_active_outlined),
+                    value: _notifBoxValue,
+                    onChanged: (String? newValue) {
+                      print(newValue);
+                      _notifBoxValue = newValue!;
+                      setState(() {
+                        _notifBoxValue;
+                      });
+                    },
+                    items: <String>['On', 'Off']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child:
+                            Text(value, style: const TextStyle(fontSize: 20)),
+                      );
+                    }).toList(),
+                  ),
+                ))),
+                ],),
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Theme', style: TextStyle(fontSize: 30)),
+                SizedBox(width: 20),
+                SizedBox(width: 250, height: 70, child:
+                InputDecorator(
+                    decoration: InputDecoration(
+                      border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
+                    ), child:
+                DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    icon: const Icon(Icons.notifications_active_outlined),
+                    value: theming.currentThemeName,
+                    onChanged: (String? newValue) {
+                      theming.currentTheme = theming.themeMapping[newValue!]!;
+                      theming.currentThemeName = newValue;
+                      setState(() {
+                        theming.currentTheme;
+                        theming.currentThemeName;
+                      });
+                      themeProvider.changeTheme(theming.currentTheme);
+                    },
+                    items: <String>['Pink', 'Green', 'Blue', 'Purple', 'Orange', 'Red']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child:
+                        Text(value, style: const TextStyle(fontSize: 20)),
+                      );
+                    }).toList(),
+                  ),
+                ))),
+              ],),
+            const SizedBox(height: 20),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text('Name', style: TextStyle(fontSize: 30)),
+                    SizedBox(width: 20),
+                    SizedBox(
+                    width: 250,
+                    child: TextField(
+                      autocorrect: false,
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Your name',
+                      ),
+                      onSubmitted: (String value) async {
+                        print(value);
+                      },
+                    )),
+                  ],),
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Partner\'s Bug Code', style: TextStyle(fontSize: 30)),
+                SizedBox(width: 20),
+                SizedBox(
+                width: 245,
+                child: TextField(
+                  autocorrect: false,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Partner\'s bug code',
+                  ),
+                  onSubmitted: (String value) async {
+                    print(value);
+                  },
+                )),
+          ],
+            ),
+            const SizedBox(height: 20),
+            //Steam Id
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Steam ID', style: TextStyle(fontSize: 30)),
+                SizedBox(width: 20),
+                SizedBox(
+                width: 250,
+                child: TextField(
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Your Steam ID',
+                  ),
+                  onSubmitted: (String value) async {
+                    print(value);
+                  },
+                )),
+          ]),
+            //Steam API Key
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Steam API Key', style: TextStyle(fontSize: 30)),
+                SizedBox(width: 20),
+                SizedBox(
+                width: 250,
+                child: TextField(
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Your Steam API Key',
+                  ),
+                  onSubmitted: (String value) async {
+                    print(value);
+                  },
+                )),
+          ]),
+            // profile picture
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Profile Picture', style: TextStyle(fontSize: 30)),
+                SizedBox(width: 20),
+                SizedBox(
+                width: 250,
+                child: ButtonBar(
+                  alignment: MainAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Upload'),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Remove'),
+                    ),
+                  ],
+                )),
+
+        ]),
+            // Additional Games
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Additional Games', style: TextStyle(fontSize: 30)),
+                SizedBox(width: 20),
+                SizedBox(
+                width: 250,
+                child: TextFormField(
+                  autocorrect: false,
+                  minLines: 6,
+                  maxLines: 10 ,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter as many games as you want, seperated by commas',
+                  ),
+                  onFieldSubmitted: (String value) async {
+                    print(value);
+                  },
+                )),
+          ]),
+            //Server URL
+            const SizedBox(height: 20),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Server URL', style: TextStyle(fontSize: 30)),
+                SizedBox(width: 20),
+                SizedBox(
+                width: 250,
+                child: TextField(
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Remote Server URL',
+                  ),
+                  onSubmitted: (String value) async {
+                    print(value);
+                  },
+                )),
+          ]),
+      ])
+      ),
+    );
+  }
+}
+
+mixin _BugCode {
+  static String BugCode = '123456';
 }
---
+++ BugInterface/lovebug/lib/networking.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'package:web_socket_channel/web_socket_channel.dart' as ws;
+class HTTP {
+  static Future<http.Response> get(String url) async {
+    http.Response res = await http.get(Uri.parse(url));
+    return res;
+  }
+  // Specialized HTTP
+  static Future<http.Response>
+}
+
+class RTC {
+  ws.WebSocketChannel? channel;
+  Future<ws.WebSocketChannel> connect(String url) async {
+    if(channel != null) {
+      return channel!;
+    }
+    channel = ws.WebSocketChannel.connect(Uri.parse(url));
+    return channel!;
+  }
+  Future<void> send(ws.WebSocketChannel channel, String message) async {
+    channel.sink.add(message);
+  }
+  Future<void> close(ws.WebSocketChannel channel) async {
+    channel.sink.close();
+  }
+
+}
